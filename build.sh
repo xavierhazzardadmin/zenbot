1353fec17d93eada36dc60eb30906ffba40e908b
Xavier Charles Hazzard <xavier@hazzard.org.uk>
2023-07-31 16:44:24 +0100

    Debugging

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 systemctl stop zenbot
+echo "Stopped zenbot"
 
 sudo go build -buildvcs=false -o /bin/zenbot/zenbot 
 
+echo "Built executable"
+
 systemctl start zenbot
+
+echo "Started zenbot"
