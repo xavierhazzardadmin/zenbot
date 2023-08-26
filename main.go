c658c09106c1ebc4c6f113c15b554cc376b10ef2
Xavier Hazzard <xavier@hazzard.org.uk>
2023-08-26 14:46:25 +0100

    Added bored command

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os/signal"
 	"strings"
 	"syscall"
+	"math/rand"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -83,4 +84,30 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if m.Author.ID == MYID && strings.ToLower(m.Content) == "!luc" {
 		s.ChannelMessageSend(m.ChannelID, "https://lh3.googleusercontent.com/pw/AIL4fc9kzv5TEvAwkO4SJcDbubGYlMfnK8KuxouW6NkC-lxHlDIJFbrLZ9wtvs94WwwdNj1ai6HmJW1BzjJvxK1l2_lMX5ZyNRVOO9-j2Imxi6DxxyB_p54BUiewzAyoJbVWneB1aQcWFJKQ6zZgWO5cO6b8ZeWEOk6duuXNiWRiIpt0_izlhxAWud0SVyB2UXN0d-tuUqYxV9M86fqnvUnyZm7OVLyXDXiZ7Gv9LXNyM9RJ05xRlHEN7jdSqQ_kBtuwB0MnuyJs_uYVzOWbxdzDwbApLoYH4l2mmb4iMAOgL1z1_MzD6HK2OPM5kbko9FDvkeymr2cWOAPYS2yayFSvvoCOIiRmfZVVq4oGRffSSGHeCV6GKhwlX6oUk2wCGN6GXIqSMObvgMd6vUi46OscDWRktrYitMiM7b35-QxlSM60hNbS22ObilMJZqOqKKTKCdAcdegjkmolopK9zeGV85jy9n5QyE_Jx3BheSdG0z9Q5Id81GkNRghGgD-GWWLTJrcuKe6Jg4NNAf0PylIJQeoqsngMg8Gfup4clwM4MhLoaWffRj6Kp_i7n2b510vTQCkIH14lUg6yaMdp9r2Xb81WVrWRagYWZAN1afqRHHpyqcvqooR-oNLfSHm6IX4d4fUKN-9LD7PBL7MsQpdvFZdCthHGQWGO9GIXuK29oLLsr7Vzow3r17WNW27fwbNq43Ne6UCxe0hKSV49CbDSeilvFcY8ezCSD3s97m_Yoa_GsWg2M4INlTbyIgihz4e4HRXMS0u8n_RhgMB4-IVHCeILnktz096MyDrI9hMLnvk6P8CBGArxBOXVVKYSyl8x_keAVLLQKYwEfFS6-14IhJw7n8ADlKZnyp3CTEOJChUzlkMLIsCoQ3KJ7LsZ6Gba1HY0hKIscxuYJAmCmTGC8g=w723-h1286-s-no?authuser=0")
 	}
+
+	if strings.ToLower(m.Content) == "!bored" {
+		reply := generateRandomIdea()
+
+		s.ChannelMessageSendReply(m.ChannelID, reply, m.Reference())
+	}
+}
+
+func generateRandomIdea() string {
+	games := []string{"Brick Rigs", "Minecraft Java", "Forza Horizon 4", "Forza Horizon 5", "Minecraft Bedrock", "No Man's Sky", "Gang Beast", "Roblox", "Rocket League", "Fall Guys", "Among Us"}
+	ideas := []string{"Go Touch Grass", "Learn to cook", "Read a book", "Work out", "Watch Youtube", "Learn something new"}
+	control1 := rand.Intn(1)
+
+	var control string
+
+	if control1 == 1 {
+		control2 := rand.Intn(len(games))
+
+		control = fmt.Sprintf("Why don't you play %s\n", games[control2])
+	} else {
+		control2 := rand.Intn(len(ideas))
+		
+		control = fmt.Sprintf("Why don't you %s\n", ideas[control2])
+	}
+
+	return control
 }
